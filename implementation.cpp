defd963cea614705cbe68029c42ed522086ed10a
Pranumishra <pranav9451mishra@gmail.com>
2024-12-20 01:10:57 +0530

    formed groups based on number of ones in binary form using map

--- implementation.cpp
+++ implementation.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #include <set>
 #include <algorithm>
 #include <string>
-#include <iomanip>
+#include <iomanip> //for input and output formatting
 
 
 string convert_to_binary(int num,int length){
@@ -25,8 +25,64 @@ string convert_to_binary(int num,int length){
     return ans;
 }
 
+int ones_count(string str){
+    int count=0;
+    for(auto ch:str){
+        if(ch=='1'){
+            count++;
+        }
+    }
+    return count;
+}
+
 int main(){
-    int num=5;
-    cout<<num<<endl;
-    cout<<"Binary Representation :"<<convert_to_binary(num,4)<<endl;
+    int variables;   //number of variables such as (a,b,c,d)=>4.
+    int m;           //to store number of min terms
+    int d;           //to store number of dont care
+
+    cout<<"Enter number of variables : ";
+    cin>>variables;
+
+    cout<<"Enter number of minTerms : ";
+    cin>>m;  
+    vector<int>minTerms(m);
+    cout<<"Enter all minTerms seprated by space : ";
+    for(int i=0;i<m;i++){
+        cin>>minTerms[i];
+    }
+
+    cout<<"Enter number of don't cares : ";
+    cin>>d;
+    vector<int>dontCare(d);
+    cout<<"Enter all don't cares seprated by space : ";
+    for(int i=0;i<d;i++){
+        cin>>dontCare[i];
+    }
+
+
+    //Combining all min terms and dont cares in single array for processing
+    vector<int>Terms(m+d);
+    int i=0;
+    for(;i<m;i++){
+        Terms[i]=minTerms[i];
+    }
+    for(;i<m+d;i++){
+        Terms[i]=dontCare[i-m];
+    }
+
+    //Forming groups according to number of one's
+    map<int,vector<string>>groups;
+    for(int num:Terms){
+        string binaryForm = convert_to_binary(num,variables);
+        int count=ones_count(binaryForm);
+        groups[count].push_back(binaryForm);
+    }
+
+    for(auto grp : groups){
+        cout<<grp.first<<" : ";
+        for(auto str:grp.second){
+            cout<<str<<" ";
+        }
+        cout<<endl;
+    }
 }
