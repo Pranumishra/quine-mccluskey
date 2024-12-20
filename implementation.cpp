f8b7f687fa945f6d71250e6313adf53f404b28ba
Pranumishra <pranav9451mishra@gmail.com>
2024-12-21 00:57:02 +0530

    changed the way to group according to number of 1 present

--- implementation.cpp
+++ implementation.cpp
@@ -9,31 +9,31 @@ using namespace std;
 #include <iomanip> //for input and output formatting
 
 
-// string convert_to_binary(int num,int length){
-//     string ans="";
-//     for(int i=length-1;i>=0;i--){
-//         int bit = num&1;
-//         if(bit){
-//             ans+='1';
-//         }
-//         else{
-//             ans+='0';
-//         }
-//         num=num>>1;
-//     }
-//     reverse(ans.begin(),ans.end());
-//     return ans;
-// }
+string convert_to_binary(int num,int length){
+    string ans="";
+    for(int i=length-1;i>=0;i--){
+        int bit = num&1;
+        if(bit){
+            ans+='1';
+        }
+        else{
+            ans+='0';
+        }
+        num=num>>1;
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
 
-// int ones_count(string str){
-//     int count=0;
-//     for(auto ch:str){
-//         if(ch=='1'){
-//             count++;
-//         }
-//     }
-//     return count;
-// }
+int ones_count(string str){
+    int count=0;
+    for(auto ch:str){
+        if(ch=='1'){
+            count++;
+        }
+    }
+    return count;
+}
 
 
 // "1-0" → A C'
@@ -52,55 +52,55 @@ string binary_to_boolean_expression(string str){
 }
 
 int main(){
-    cout<<"1-00"<<endl;
-    cout<<binary_to_boolean_expression("0-00")<<endl;
-    // int variables;   //number of variables such as (a,b,c,d)=>4.
-    // int m;           //to store number of min terms
-    // int d;           //to store number of dont care
 
-    // cout<<"Enter number of variables : ";
-    // cin>>variables;
+    int variables;   //number of variables such as (a,b,c,d)=>4.
+    int m;           //to store number of min terms
+    int d;           //to store number of dont care
 
-    // cout<<"Enter number of minTerms : ";
-    // cin>>m;  
-    // vector<int>minTerms(m);
-    // cout<<"Enter all minTerms seprated by space : ";
-    // for(int i=0;i<m;i++){
-    //     cin>>minTerms[i];
-    // }
+    cout<<"Enter number of variables : ";
+    cin>>variables;
 
-    // cout<<"Enter number of don't cares : ";
-    // cin>>d;
-    // vector<int>dontCare(d);
-    // cout<<"Enter all don't cares seprated by space : ";
-    // for(int i=0;i<d;i++){
-    //     cin>>dontCare[i];
-    // }
+    cout<<"Enter number of minTerms : ";
+    cin>>m;  
+    vector<int>minTerms(m);
+    cout<<"Enter all minTerms seprated by space : ";
+    for(int i=0;i<m;i++){
+        cin>>minTerms[i];
+    }
 
+    cout<<"Enter number of don't cares : ";
+    cin>>d;
+    vector<int>dontCare(d);
+    cout<<"Enter all don't cares seprated by space : ";
+    for(int i=0;i<d;i++){
+        cin>>dontCare[i];
+    }
+    cout<<endl;
 
-    // //Combining all min terms and dont cares in single array for processing
-    // vector<int>Terms(m+d);
-    // int i=0;
-    // for(;i<m;i++){
-    //     Terms[i]=minTerms[i];
-    // }
-    // for(;i<m+d;i++){
-    //     Terms[i]=dontCare[i-m];
-    // }
+    //Combining all min terms and dont cares in single array for processing
+    vector<int>Terms(m+d);
+    int i=0;
+    for(;i<m;i++){
+        Terms[i]=minTerms[i];
+    }
+    for(;i<m+d;i++){
+        Terms[i]=dontCare[i-m];
+    }
 
-    // //Forming groups according to number of one's
-    // map<int,vector<string>>groups;
-    // for(int num:Terms){
-    //     string binaryForm = convert_to_binary(num,variables);
-    //     int count=ones_count(binaryForm);
-    //     groups[count].push_back(binaryForm);
-    // }
+    //Forming groups according to number of one's
+    map<int,vector<pair<int,string>>>groups;
+    for(int num:Terms){
+        string binaryForm = convert_to_binary(num,variables);
+        int count=ones_count(binaryForm);
+        groups[count].push_back({num,binaryForm});
+    }
 
-    // for(auto grp : groups){
-    //     cout<<grp.first<<" : ";
-    //     for(auto str:grp.second){
-    //         cout<<str<<" ";
-    //     }
-    //     cout<<endl;
-    // }
+    for(auto grp : groups){
+        cout<<"Group "<<grp.first<<":- ";
+        cout<<endl;
+        for(auto str:grp.second){
+            cout<<str.first<<" : "<<str.second<<endl;
+        }
+        cout<<endl;
+    }
 }
